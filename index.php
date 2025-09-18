0321fdbb66fc27aac04f413f4a5b592eca1c70da
Javica <franciscojaviercasado001.alu@stemgranada.com>
2025-09-18 09:04:02 +0200

    feat: createe index prueba

---
+++ index.php
@@ -0,0 +1,9 @@
+
+<?php
+$nombre = "Javica";
+echo "<h1>Hola, $nombre!</h1>";
+echo "<h2>Hola, $nombre!</h2>";
+
+// diferencia con comilla simple  a la doble
+echo '<h3>Hola, '.$nombre.'!</h3>';
+?>
